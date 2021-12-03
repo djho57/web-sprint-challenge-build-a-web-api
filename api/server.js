839b3a1bbf1232ad9fbecded2f001205090319cb
djho57 <daniel.j.ho57@gmail.com>
2021-12-03 14:33:04 -0600

    mvp

--- api/server.js
+++ api/server.js
@@ -1,9 +1,31 @@
-const express = require('express');
+/* eslint-disable */
+const express = require("express");
 const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+server.use(express.json());
+
+const actionsRouter = require("./actions/actions-router");
+const projectsRouter = require("./projects/projects-router");
+
+server.use("/api/actions", actionsRouter);
+server.use("/api/projects", projectsRouter);
+
+server.get("/", (req, res) => {
+  res.send(`we here!`);
+});
+server.get("/api", (req, res) => {
+  res.json({ message: "api is working" });
+});
+server.get("/api/projects", (req, res) => {
+  res.json({ message: "projects here" });
+});
+server.get("/api/actions", (req, res) => {
+  res.json({ message: "actions here" });
+});
+
+server.use(function (req, res) {
+  res.status(404).send("404 - This page not found");
+});
 
 module.exports = server;
